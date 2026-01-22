794a93f6f48f77b943b5bafd59c82abef95b1691
o-az <omaraziz.dev@proton.me>
2026-01-22 01:24:35 -0800

    chore: pull from gh

--- src/routes/posts/{$slug}[.]md.ts
+++ src/routes/posts/{$slug}[.]md.ts
@@ -1,16 +1,16 @@
 import { createFileRoute } from '@tanstack/solid-router'
 
-import { allPosts } from '#content-collections'
+const RAW_URL = 'https://raw.githubusercontent.com/o-az/omar.mov/main/src/content/posts'
 
 export const Route = createFileRoute('/posts/{$slug}.md')({
   server: {
     handlers: {
       GET: async ({ params }) => {
-        const post = allPosts.find(item => item.slug === params.slug)
+        const response = await fetch(`${RAW_URL}/${params.slug}/index.mdx`)
 
-        if (!post) return new Response('Not found', { status: 404 })
+        if (!response.ok) return new Response('Not found', { status: 404 })
 
-        return new Response(post.content, {
+        return new Response(response.body, {
           headers: { 'content-type': 'text/markdown; charset=utf-8' }
         })
       }
